0411d7df75b71af0106734fbb41e1c6e4461eaf1
aeger <almty1@gmail.com>
2022-09-05 21:31:02 -0700

    Add project files.

---
+++ NewMod/Plugin.cs
@@ -0,0 +1,147 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using PieceManager;
+using ServerSync;
+
+namespace NewMod
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class NewModPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "NewMod";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "almty1";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+        internal static string ConnectionError = "";
+        private readonly Harmony _harmony = new(ModGUID);
+
+        public static readonly ManualLogSource NewModLogger =
+            BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        private static readonly ConfigSync ConfigSync = new(ModGUID)
+            { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+
+        public void Awake()
+        {
+            _serverConfigLocked = config("General", "Force Server Config", true, "Force Server Config");
+            _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
+
+            // Globally turn off configuration options for your pieces, omit if you don't want to do this.
+            BuildPiece.ConfigurationEnabled = false;
+            
+            // Format: new("AssetBundleName", "PrefabName", "FolderName");
+            BuildPiece examplePiece1 = new("funward", "funward", "FunWard");
+
+            examplePiece1.Name.English("Fun Ward"); // Localize the name and description for the building piece for a language.
+            examplePiece1.Description.English("Ward For testing the Piece Manager");
+            examplePiece1.RequiredItems.Add("FineWood", 20, false); // Set the required items to build. Format: ("PrefabName", Amount, Recoverable)
+            examplePiece1.RequiredItems.Add("SurtlingCore", 20, false);
+            examplePiece1.Category.Add(BuildPieceCategory.Misc);
+            examplePiece1.Crafting.Set(CraftingTable.ArtisanTable); // Set a crafting station requirement for the piece.
+            //examplePiece1.Crafting.Set("CUSTOMTABLE"); // If you have a custom table you're adding to the game. Just set it like this.
+
+            //examplePiece1.SpecialProperties.NoConfig = true;  // Do not generate a config for this piece, omit this line of code if you want to generate a config.
+            examplePiece1.SpecialProperties = new SpecialProperties() { AdminOnly = true, NoConfig = true}; // You can declare multiple properties in one line           
+
+
+            BuildPiece examplePiece2 = new("bamboo", "Bamboo_Wall"); // Note: If you wish to use the default "assets" folder for your assets, you can omit it!
+            examplePiece2.Name.English("Bamboo Wall");
+            examplePiece2.Description.English("A wall made of bamboo!");
+            examplePiece2.RequiredItems.Add("BambooLog", 20, false);
+            examplePiece2.Category.Add(BuildPieceCategory.Building);
+            examplePiece2.Crafting.Set("CUSTOMTABLE"); // If you have a custom table you're adding to the game. Just set it like this.
+            examplePiece2.SpecialProperties.AdminOnly = true;  // You can declare these one at a time as well!.
+
+
+            // If you want to add your item to the cultivator or another hammer with vanilla categories
+            // Format: (AssetBundle, "PrefabName", addToCustom, "Item that has a piecetable")
+            BuildPiece examplePiece3 = new(PiecePrefabManager.RegisterAssetBundle("bamboo"), "Bamboo_Sapling", true, "Cultivator");
+            examplePiece3.Name.English("Bamboo Sapling");
+            examplePiece3.Description.English("A young bamboo tree, called a sapling");
+            examplePiece3.RequiredItems.Add("BambooSeed", 20, false);
+            examplePiece3.SpecialProperties.NoConfig = true;
+
+            // Need to add something to ZNetScene but not the hammer, cultivator or other? 
+            PiecePrefabManager.RegisterPrefab("bamboo", "Bamboo_Beam_Light");
+            
+            // Does your model need to swap materials with a vanilla material? Format: (GameObject, isJotunnMock)
+            MaterialReplacer.RegisterGameObjectForMatSwap(examplePiece3.Prefab, false);
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+            SetupWatcher();
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                NewModLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                NewModLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                NewModLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+
+        #region ConfigOptions
+
+        private static ConfigEntry<bool>? _serverConfigLocked;
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription =
+                new(
+                    description.Description +
+                    (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                    description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            public bool? Browsable = false;
+        }
+
+        #endregion
+    }
+}
---
+++ NewMod/VersionHandshake.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+
+namespace NewMod
+{
+    [HarmonyPatch(typeof(ZNet), nameof(ZNet.OnNewConnection))]
+    public static class RegisterAndCheckVersion
+    {
+        private static void Prefix(ZNetPeer peer, ref ZNet __instance)
+        {
+            // Register version check call
+            NewModPlugin.NewModLogger.LogDebug("Registering version RPC handler");
+            peer.m_rpc.Register($"{NewModPlugin.ModName}_VersionCheck",
+                new Action<ZRpc, ZPackage>(RpcHandlers.RPC_NewMod_Version));
+
+            // Make calls to check versions
+            NewModPlugin.NewModLogger.LogInfo("Invoking version check");
+            ZPackage zpackage = new();
+            zpackage.Write(NewModPlugin.ModVersion);
+            peer.m_rpc.Invoke($"{NewModPlugin.ModName}_VersionCheck", zpackage);
+        }
+    }
+
+    [HarmonyPatch(typeof(ZNet), nameof(ZNet.RPC_PeerInfo))]
+    public static class VerifyClient
+    {
+        private static bool Prefix(ZRpc rpc, ZPackage pkg, ref ZNet __instance)
+        {
+            if (!__instance.IsServer() || RpcHandlers.ValidatedPeers.Contains(rpc)) return true;
+            // Disconnect peer if they didn't send mod version at all
+            NewModPlugin.NewModLogger.LogWarning(
+                $"Peer ({rpc.m_socket.GetHostName()}) never sent version or couldn't due to previous disconnect, disconnecting");
+            rpc.Invoke("Error", 3);
+            return false; // Prevent calling underlying method
+        }
+
+        private static void Postfix(ZNet __instance)
+        {
+            ZRoutedRpc.instance.InvokeRoutedRPC(ZRoutedRpc.instance.GetServerPeerID(), "RequestAdminSync",
+                new ZPackage());
+        }
+    }
+
+    [HarmonyPatch(typeof(FejdStartup), nameof(FejdStartup.ShowConnectError))]
+    public class ShowConnectionError
+    {
+        private static void Postfix(FejdStartup __instance)
+        {
+            if (__instance.m_connectionFailedPanel.activeSelf)
+                __instance.m_connectionFailedError.text += "\n" + NewModPlugin.ConnectionError;
+        }
+    }
+
+    [HarmonyPatch(typeof(ZNet), nameof(ZNet.Disconnect))]
+    public static class RemoveDisconnectedPeerFromVerified
+    {
+        private static void Prefix(ZNetPeer peer, ref ZNet __instance)
+        {
+            if (!__instance.IsServer()) return;
+            // Remove peer from validated list
+            NewModPlugin.NewModLogger.LogInfo(
+                $"Peer ({peer.m_rpc.m_socket.GetHostName()}) disconnected, removing from validated list");
+            _ = RpcHandlers.ValidatedPeers.Remove(peer.m_rpc);
+        }
+    }
+
+    public static class RpcHandlers
+    {
+        public static readonly List<ZRpc> ValidatedPeers = new();
+
+        public static void RPC_NewMod_Version(ZRpc rpc, ZPackage pkg)
+        {
+            string? version = pkg.ReadString();
+            NewModPlugin.NewModLogger.LogInfo("Version check, local: " +
+                                                                   NewModPlugin.ModVersion +
+                                                                   ",  remote: " + version);
+            if (version != NewModPlugin.ModVersion)
+            {
+                NewModPlugin.ConnectionError =
+                    $"{NewModPlugin.ModName} Installed: {NewModPlugin.ModVersion}\n Needed: {version}";
+                if (!ZNet.instance.IsServer()) return;
+                // Different versions - force disconnect client from server
+                NewModPlugin.NewModLogger.LogWarning(
+                    $"Peer ({rpc.m_socket.GetHostName()}) has incompatible version, disconnecting");
+                rpc.Invoke("Error", 3);
+            }
+            else
+            {
+                if (!ZNet.instance.IsServer())
+                {
+                    // Enable mod on client if versions match
+                    NewModPlugin.NewModLogger.LogInfo("Received same version from server!");
+                }
+                else
+                {
+                    // Add client to validated list
+                    NewModPlugin.NewModLogger.LogInfo(
+                        $"Adding peer ({rpc.m_socket.GetHostName()}) to validated list");
+                    ValidatedPeers.Add(rpc);
+                }
+            }
+        }
+    }
+}
